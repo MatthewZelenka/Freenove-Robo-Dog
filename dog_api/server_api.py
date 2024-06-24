364786c9cb547c94bd66b2e8318f488c30ca2214
Matthew Zelenka <mzelenka04@gmail.com>
2024-06-24 01:38:54 -0400

    added video

--- dog_api/server_api.py
+++ dog_api/server_api.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import io
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Response
 from contextlib import asynccontextmanager
 import asyncio
 import RPi.GPIO as GPIO
+from picamera import PiCamera
 
 from connection_manager import ConnectionManager
 from RPi_ultrasonic import ultrasonic
 
+"""
+Setting up variables
+"""
+
 manager = ConnectionManager()
 
-ultrasonic_sensor = ultrasonic(trigger_pin=27, echo_pin=22, GPIO_Mode=GPIO.BCM)
-
+"""
+Set up fastapi server
+"""
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # On startup
@@ -19,6 +27,17 @@ async def lifespan(app: FastAPI):
     pass
 
 app = FastAPI(lifespan=lifespan)
+
+"""
+Functions
+"""
+
+
+"""
+Ultrasonic sensor api
+"""
+
+ultrasonic_sensor = ultrasonic(trigger_pin=27, echo_pin=22, GPIO_Mode=GPIO.BCM)
 
 async def read_ultrasonic_sensor():
     """
@@ -46,6 +65,34 @@ async def broadcast_sensor_data():
     async for distance in read_ultrasonic_sensor():
         if manager.has_active_connections():
             await manager.broadcast(str(distance))
+
+"""
+Camera stream
+"""
+# initialize the PiCamera
+camera = PiCamera()
+
+# camera settings
+camera.resolution = (400, 300)
+camera.framerate = 24
+
+# create a circular buffer to store latest frame
+stream = io.BytesIO()
+
+# Function to continuously capture frames from camera
+def capture_frames():
+    global stream
+    for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+        # Rewind the stream ready for reading
+        stream.seek(0)
+        yield stream.read()
+        # Reset stream for next frame
+        stream.seek(0)
+        stream.truncate()
+
+@app.get("/camera")
+async def video_feed():
+    return Response(content=capture_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
 
 # Optional endpoint to check if the server is running
 @app.get("/")
