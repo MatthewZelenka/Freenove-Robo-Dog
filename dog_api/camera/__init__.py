3b1cc632bf3167673f21345d2c1d5d8f1ec9dabb
Matthew Zelenka <mzelenka04@gmail.com>
2024-07-23 19:11:58 -0400

    camera stuff

--- dog_api/camera/__init__.py
+++ dog_api/camera/__init__.py
@@ -1,7 +1,7 @@
-
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 import asyncio
 import cv2
+from picamera2 import Picamera2
 
 from ..connection_manager import ConnectionManager
 
@@ -12,23 +12,28 @@ camera_manager = ConnectionManager()
 """
 Camera api
 """
-
+ 
 FPS = 30
-
-async def read_camera(cap:cv2.VideoCapture):
+ 
+async def read_camera(picam2:Picamera2):
     """
     Function to reading data from an camera
     """
-    while True:
-        await asyncio.sleep(1/FPS)  # Delay between readings
-        
-        ret, frame = cap.read()
-        if not ret:
-            break
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield frame
-
+    await asyncio.sleep(1 / FPS)  # Delay between readings
+    
+    # Capture an image
+    frame = picam2.capture_array()
+ 
+    # Convert the image to BGR format if needed
+    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+ 
+    # Encode the frame as JPEG
+    _, buffer = cv2.imencode('.jpg', frame_bgr)
+    frame_bytes = buffer.tobytes()
+            
+    # Yield the frame bytes
+    yield frame_bytes
+ 
 @router.websocket("/camera")
 async def camera_websocket_endpoint(websocket: WebSocket):
     await camera_manager.connect(websocket)
@@ -38,29 +43,29 @@ async def camera_websocket_endpoint(websocket: WebSocket):
             await camera_manager.send_personal_message(f"Message text was: {data}", websocket)
     except WebSocketDisconnect:
         camera_manager.disconnect(websocket)
-
+ 
 async def broadcast_camera_data():
     """
     Background task to broadcast sensor data to all connected clients
     """
-    cap: cv2.VideoCapture 
-
+    picam2 = Picamera2()
+    video_config = picam2.create_video_configuration(main={"size": (640, 480)})
+    picam2.configure(video_config)
     camera_status: bool = False
-
+ 
     while True:
         if camera_manager.has_active_connections():
             if not camera_status:
-                cap = cv2.VideoCapture(0)
+                picam2.start()
                 camera_status = True
             else:
-                async for frame in read_camera(cap):
+                async for frame in read_camera(picam2):
                     if camera_manager.has_active_connections():
                         await camera_manager.broadcast_bytes(frame)
                     else:
                         break
         else:
-            
             if camera_status:
-                cap.release()
+                picam2.stop()
                 camera_status = False
             await asyncio.sleep(1)  # Check for active connections periodically
